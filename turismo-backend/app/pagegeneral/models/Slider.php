bd7730032fef89df67828ee0983985d88919db12
TheReeds <tomas159357852@gmail.com>
2025-04-10 15:48:25 -0500

    Bear

---
+++ turismo-backend/app/pagegeneral/models/Slider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\pagegeneral\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Slider extends Model
+{
+
+    protected $fillable = [
+        'id',
+        'name',
+    ];
+}
