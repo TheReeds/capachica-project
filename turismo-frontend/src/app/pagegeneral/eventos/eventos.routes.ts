2e3b4720b1322d2186180d60c7e909d4767fcea5
TheReeds <tomas159357852@gmail.com>
2025-06-14 17:58:13 -0500

    functional

--- turismo-frontend/src/app/pagegeneral/eventos/eventos.routes.ts
+++ turismo-frontend/src/app/pagegeneral/eventos/eventos.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { EventosComponent } from './eventos.component';
-import { DetallefamiliasComponent } from '../pagegeneral.routes';
 import { EventosdetalleComponent } from './eventosdetalle/eventosdetalle.component';
 
 export const EVENTOS_ROUTES: Routes = [
